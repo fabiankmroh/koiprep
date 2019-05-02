837102c8465b7fa853c2a62e64c9ce5b5d0110f1
Fabian Roh <fabiankmroh@gmail.com>
2019-05-02 21:50:19 +0900

    UNSOLVED: Lack of Time

---
+++ 2019/27cs.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int course[1000];
+int accu[1000][3];
+int score[1000];
+int opp; // opportunity
+int i, n;
+int max, coordinate;
+
+int main(void){
+	scanf("%d", &n);
+	
+	for(i = 0; i < n; i++){
+		scanf("%d", &course[i]);	
+	}
+	
+	scanf("%d", &opp);
+	
+	for(i = 0; i < n; i++){
+        
+    }
+}
+	
